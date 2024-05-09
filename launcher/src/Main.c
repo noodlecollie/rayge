2da96f4093518126d940288204f352b3389bcfff
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-05-09 19:22:37 +0100

    Allowed loading either debug or release engine

--- launcher/src/Main.c
+++ launcher/src/Main.c
@@ -1,9 +1,58 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "RayGE/Private/Launcher.h"
 #include "RayGE/Private/InterfaceUtils.h"
 #include "RayGE/Platform.h"
 #include "wzl_cutl/libloader.h"
-#include <stdio.h>
+#include "wzl_cutl/string.h"
+
+#ifndef NDEBUG
+// Debug
+#define FIRST_ENGINE_PREFERENCE LIB_PREFIX LIBNAME_ENGINE_DEBUG LIB_EXTENSION
+#define SECOND_ENGINE_PREFERENCE LIB_PREFIX LIBNAME_ENGINE LIB_EXTENSION
+#define OPTION_SWAP_ENGINE_PREF "--prefer-release-engine"
+#else
+// Release
+#define FIRST_ENGINE_PREFERENCE LIB_PREFIX LIBNAME_ENGINE LIB_EXTENSION
+#define SECOND_ENGINE_PREFERENCE LIB_PREFIX LIBNAME_ENGINE_DEBUG LIB_EXTENSION
+#define OPTION_SWAP_ENGINE_PREF "--prefer-debug-engine"
+#endif
+
+static const char* g_EngineLibPaths[2] =
+{
+	FIRST_ENGINE_PREFERENCE,
+	SECOND_ENGINE_PREFERENCE,
+};
+
+static void SwapEngineLibPathPreference(void)
+{
+	static bool haveSwappedPrefs = false;
+
+	if ( haveSwappedPrefs )
+	{
+		return;
+	}
+
+	const char* temp = g_EngineLibPaths[0];
+	g_EngineLibPaths[0] = g_EngineLibPaths[1];
+	g_EngineLibPaths[1] = temp;
+
+	haveSwappedPrefs = true;
+}
+
+static void* LoadEngineLibrary(void)
+{
+	printf("Attempting to load engine library: %s\n", g_EngineLibPaths[0]);
+	void* engineLibrary = wzl_load_library(g_EngineLibPaths[0]);
+
+	if ( engineLibrary )
+	{
+		return engineLibrary;
+	}
+
+	printf("Failed to load %s, falling back to %s\n", g_EngineLibPaths[0], g_EngineLibPaths[1]);
+	return wzl_load_library(g_EngineLibPaths[1]);
+}
 
 int main(int argc, char** argv)
 {
@@ -12,11 +61,18 @@ int main(int argc, char** argv)
 		return RAYGE_LAUNCHER_EXIT_UNKNOWN_ERROR;
 	}
 
-	void* engineLibrary = wzl_load_library(LIB_PREFIX LIBNAME_ENGINE LIB_EXTENSION);
+	for ( int index = 1; index < argc; ++index )
+	{
+		if ( wzl_strequals(argv[index], OPTION_SWAP_ENGINE_PREF) )
+		{
+			SwapEngineLibPathPreference();
+		}
+	}
+
+	void* engineLibrary = LoadEngineLibrary();
 
 	if ( !engineLibrary )
 	{
-		// TODO: More comprehensive function calls
 		fprintf(stderr, "Failed to load engine library\n");
 		return RAYGE_LAUNCHER_EXIT_FAIL_ENGINE_LOAD;
 	}
@@ -26,18 +82,40 @@ int main(int argc, char** argv)
 
 	if ( !runFuncPtr )
 	{
-		// TODO: More comprehensive function calls
 		fprintf(stderr, "Failed to look up function in engine library\n");
 		return RAYGE_LAUNCHER_EXIT_FAIL_ENGINE_LOAD;
+	}
+
+	// Build a new param list, excluding any consumed params.
+	// Apparently we need an extra null param on the end,
+	// or cargs doesn't know where the end is...
+	// (Surely it should use argc??)
+	char** newParams = (char**)calloc(argc + 1, sizeof(char*));
+	size_t newParamCount = 0;
+
+	for ( int index = 0; index < argc; ++index )
+	{
+		if ( !wzl_strequals(argv[index], OPTION_SWAP_ENGINE_PREF) )
+		{
+			newParams[newParamCount++] = wzl_strdup(argv[index]);
+		}
 	}
 
 	RayGE_LaunchParams params;
 	RAYGE_INTERFACE_INIT(RayGE_LaunchParams, &params, RAYGE_LAUNCHPARAMS_VERSION);
 
-	params.argc = (size_t)argc;
-	params.argv = (const char* const*)argv;
+	params.argc = newParamCount;
+	params.argv = (const char* const*)newParams;
 
 	const int32_t result = runFuncPtr(&params);
+
+	// Free the new param list
+	for ( size_t index = 0; index < newParamCount; ++index )
+	{
+		free(newParams[index]);
+	}
+
+	free(newParams);
 
 	wzl_unload_library(engineLibrary);
 
