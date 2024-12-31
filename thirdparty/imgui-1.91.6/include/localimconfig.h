527c8f3736ee32cc691b1ad2fb9deaa58ffc5053
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-12-31 16:01:13 +0000

    ImGui seems to build

---
+++ thirdparty/imgui-1.91.6/include/localimconfig.h
@@ -0,0 +1,9 @@
+// See ImGui's imconfig.h for the full set of options that can be configured here.
+
+#pragma once
+
+// TODO: Allow this to call through to the engine.
+//#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
+//#define IM_DEBUG_BREAK  __debugbreak()
+
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
